5460bf1b5fadaebfa17c3a215c0bc2857c40fd89
MuzLin <1030996014@qq.com>
2018-11-23 16:15:41 +0800

    使用chai添加button的4个测试用例

--- src/app.js
+++ src/app.js
@@ -14,3 +14,74 @@ new Vue({
     }
   }
 })
+
+// 单元测试
+import chai from 'chai'
+const expect = chai.expect
+{
+  // vue构造器构造一个函数
+  const Constructor = Vue.extend(Button)
+  // 实例化button (可以直接使用 $mount)
+  const mzButton = new Constructor({
+    propsData: {
+      icon: 'up'
+    }
+  }).$mount()
+  let useElement = mzButton.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-up')
+  mzButton.$el.remove()
+  mzButton.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  // vue构造器构造一个函数
+  const Constructor = Vue.extend(Button)
+  // 实例化button (可以直接使用 $mount)
+  const mzButton = new Constructor({
+    propsData: {
+      icon: 'up'
+    }
+  }).$mount(div)
+  let svg = mzButton.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq('0')
+  mzButton.$el.remove()
+  mzButton.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  // vue构造器构造一个函数
+  const Constructor = Vue.extend(Button)
+  // 实例化button (可以直接使用 $mount)
+  const mzButton = new Constructor({
+    propsData: {
+      icon: 'up',
+      position: 'right'
+    }
+  }).$mount(div)
+  let svg = mzButton.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  mzButton.$el.remove()
+  mzButton.$destroy()
+}
+{
+  // vue构造器构造一个函数
+  const Constructor = Vue.extend(Button)
+  // 实例化button (可以直接使用 $mount)
+  const mzButton = new Constructor({
+    propsData: {
+      icon: 'good'
+    }
+  }).$mount()
+  mzButton.$on('click', function(){
+    expect(1).to.eq(1)
+  })
+  let button = mzButton.$el
+  button.click()
+  mzButton.$el.remove()
+  mzButton.$destroy()
+}
