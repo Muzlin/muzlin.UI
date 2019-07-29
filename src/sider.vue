59a864c2cdc3fd68313cfe3e6536a314b9ade071
MuzLin <muzlingm@gmail.com>
2019-07-29 14:28:24 +0800

    添加sider 简单动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,15 +1,41 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-  </div>
+  <transition name="slide">
+    <div class="sider" v-if="visible">
+      <slot></slot>
+      <button class="button" @click="visible = false">close</button>
+    </div>
+  </transition>
 </template>
 
 <script>
   export default {
-    name: 'mz-sider'
+    name: 'mz-sider',
+    data() {
+      return {
+        visible: true
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+  .sider {
+    position: relative;
 
+    >.button {
+      position: absolute;
+      right: 0;
+      top: 0;
+    }
+  }
+
+  .slide-enter-active,
+  .slide-leave-active {
+    transition: all .5s;
+  }
+
+  .slide-enter,
+  .slide-leave-to {
+    margin-left: -200px;
+  }
 </style>
