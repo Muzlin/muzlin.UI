b0f33dfe78d4e5aae247f784e605951f3d2d97f8
MuzLin <muzlingm@gmail.com>
2019-12-26 23:36:49 +0800

    构建工具升级为vue cli 3.x

---
+++ src/demo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="demo">
+    <mz-button>哈哈</mz-button>
+  </div>
+</template>
+<script>
+import Button from './button'
+export default {
+  name: 'demo',
+  components: {
+    'mz-button': Button
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
