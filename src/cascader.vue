356de9ee9e57a8df548b034cefb278ff8622b249
MuzLin <muzlingm@gmail.com>
2019-12-27 01:10:08 +0800

    创建cascader 组件

---
+++ src/cascader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="cascader">
+    <div class="trigger">
+      <slot></slot>
+    </div>
+    <div class="popover">
+      <div v-for="(item, index) in source" :key="index">
+        <CascaderItem :sourceItem="item"></CascaderItem>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import CascaderItem from './cascader-item'
+export default {
+  name: 'mz-cascader',
+  components: {
+    CascaderItem
+  },
+  props: {
+    source: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  @import "var";
+  .cascader{
+    border: 1px solid;
+  }
+</style>
--- src/demo.vue
+++ src/demo.vue
@@ -1,21 +1,73 @@
 <template>
   <div class="demo">
-    <mz-button @click="tt">哈哈</mz-button>
+    <mz-button>哈哈</mz-button>
+    <mz-cascader :source="source"></mz-cascader>
   </div>
 </template>
 <script>
 import Button from './button'
+import Cascader from './cascader'
 export default {
   name: 'demo',
   components: {
-    'mz-button': Button
+    'mz-button': Button,
+    'mz-cascader': Cascader
+  },
+  data () {
+    return {
+      source: [{
+        name: '四川',
+        children: [
+          { name: '成都', children: [
+            { name: '高新区' },
+            { name: '武侯区' }
+          ] },
+          { name: '广安', children: [
+            { name: '广安区' },
+            { name: '华蓥市' }
+          ] },
+          { name: '攀枝花', children: [
+            { name: '仁和' },
+            { name: '东区' }
+          ] }
+        ]
+      }, {
+        name: '浙江',
+        children: [
+          { name: '杭州', children: [
+            { name: '嘉欣' },
+            { name: '南部' }
+          ]  },
+          { name: '上城', children: [
+            { name: '嘉禾区' },
+            { name: '南阳区' }
+          ]  }
+        ]
+      }, {
+        name: '云南',
+        children: [
+          { name: '昆明', children: [
+            { name: '北部区' },
+            { name: '高新区' }
+          ]  },
+          { name: '西昌', children: [
+            { name: '西昌南' },
+            { name: '西昌北' }
+          ]  },
+          { name: '楚雄', children: [
+            { name: '安平区' },
+            { name: '北疆区' }
+          ]  }
+        ]
+      }]
+    }
   },
   methods: {
-    tt() {
-      this.$toast('hha')
-    }
   }
 }
 </script>
 <style lang="scss" scoped>
+  .demo{
+    min-height: 100vh;
+  }
 </style>
